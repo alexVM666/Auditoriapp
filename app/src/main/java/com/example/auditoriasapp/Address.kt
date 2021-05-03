e3c3cde578c24d19dee71ae3c48387c2117929d7
alexVM666 <alexvegacaz10@gmail.com>
2021-05-02 20:06:54 -0500

    listo 4 final

--- app/src/main/java/com/example/auditoriasapp/Address.kt
+++ app/src/main/java/com/example/auditoriasapp/Address.kt
@@ -3,5 +3,6 @@ package com.example.auditoriasapp
 class Address {
     companion object{
         val IP = "https://seguroscoord.000webhostapp.com/"
+        //val IP = "http://192.168.1.7/"
     }
 }
